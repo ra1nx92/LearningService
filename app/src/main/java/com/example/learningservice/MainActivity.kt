0c3ddd19a121cbaf711a39c51553eb9204d3aa8f
ra1nx92 <ra1nx1992@gmail.com>
2021-11-17 12:16:19 +0300

    show notification

--- app/src/main/java/com/example/learningservice/MainActivity.kt
+++ app/src/main/java/com/example/learningservice/MainActivity.kt
@@ -1,7 +1,15 @@
 package com.example.learningservice
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import android.widget.Toast.LENGTH_LONG
+import androidx.core.app.NotificationCompat
+import androidx.core.content.getSystemService
 import com.example.learningservice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,9 +20,54 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        binding.btnStartService.setOnClickListener{
+        initBtn()
+    }
+
+
+// начиная с API 26 Android 8 необходимо уведомлять пользователя о фоновой работе сервиса.
+// На протяжении всей его работы должно висеть уведомление
+//метод для показа уведомления о фоновой работе сервиса
+    private fun showNotification() {
+    //за отображение уведомления отвечает класс notificationManager
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            //начиная с 26 API для уведомления необходимо создать канал
+            val notificationChannel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+        //создание уведомления
+//Можно присвоить класс Notification и передать только контекст, но тогда необходимо добавлять
+//проверку версий, т.к канал нужен только с API 26. NotificationCompat делает проверку сам
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("Title")
+            .setContentText("Text")
+            .setSmallIcon(R.drawable.ic_android_black_24dp) //если не поставить иконку, будет краш
+            .build()
+
+        notificationManager.notify(1, notification)
+    }
+
+    private fun initBtn() {
+        binding.btnStartService.setOnClickListener {
             startService(MyService.newIntent(this))
             //чтобы запустить сервис ему нужно передать интент в качестве параметра
+            showNotification()
         }
+        binding.btnForegroundService.setOnClickListener {
+            showNotification()
+        }
+        binding.btnStopService.setOnClickListener {
+            stopService(MyService.newIntent(this))
+            Toast.makeText(this,"STOP SERVICE",LENGTH_LONG).show()
+        }
+    }
+
+    companion object {
+        private const val CHANNEL_ID = "channel_id"
+        private const val CHANNEL_NAME = "channel_name"
     }
 }
--- app/src/main/java/com/example/learningservice/MyService.kt
+++ app/src/main/java/com/example/learningservice/MyService.kt
@@ -9,7 +9,7 @@ import kotlinx.coroutines.*
 //Сервисы предназначены для выполнения каких либо задач в фоне. Чтобы создать сервис, нужно
 // унаследоваться от класса Service. У него 3 метода ЖЦ. По умолчанию сервис работает на главном потоке
 //сервис как одна из основных сущностей системы Андроид, должна быть зарегестрирована в манифесте
-class MyService():Service(){
+class MyService():Service() {
 
     private val scope = CoroutineScope(Dispatchers.Main)
 
@@ -21,7 +21,7 @@ class MyService():Service(){
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         log("onStartCommand")
         scope.launch {
-            for(i in 0 until 100){
+            for (i in 0 until 100) {
                 delay(1000)
                 log("Timer $i")
             }
@@ -29,6 +29,8 @@ class MyService():Service(){
 //возвращение START_STICKY запустит работу сервиса заново, если система его уничтожит, но без стартового интента
 // START_NOT_STICKY не будет запускать сервис после уничтожения
 //START_REDELIVER_INTENT запустит сервис заново, вместе с интентом который был использован при запуске сервиса
+// начиная с API 26 Android 8 необходимо уведомлять пользователя о фоновой работе сервиса.
+// На протяжении всей его работы должно висеть уведомление
         return START_REDELIVER_INTENT
     }
 
@@ -38,16 +40,19 @@ class MyService():Service(){
         log("onDestroy")
         //stopService(newIntent(this))
     }
+
     override fun onBind(p0: Intent?): IBinder? {
         TODO("Not yet implemented")
     }
-    private fun log(message: String){
+
+    private fun log(message: String) {
         Log.d("SERVICE_TAG", "MyService $message")
     }
-//чтобы запустить сервис ему нужно передать интент в качестве параметра
-    companion object{
-        fun newIntent(context: Context):Intent{
-            return Intent(context,MyService::class.java)
+
+    //чтобы запустить сервис ему нужно передать интент в качестве параметра
+    companion object {
+        fun newIntent(context: Context): Intent {
+            return Intent(context, MyService::class.java)
         }
     }
 }
